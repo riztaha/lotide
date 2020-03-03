86971fd3a80ab98f5f53b90f299037266b6aa463
Taha Rizvi <riztaha@gmail.com>
2020-03-03 00:05:11 +0000

    Tail assignment

---
+++ tail.js
@@ -0,0 +1,30 @@
+let emoji = String.fromCodePoint(128512);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const tail = function(arr) {
+  const newArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    let result = arr[i]; // Hello
+    newArr.push(result);
+  }
+  
+  return newArr;
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
