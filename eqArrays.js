741f4cf6fe6b82b406670144930aa120d64adfb1
Taha Rizvi <riztaha@gmail.com>
2020-03-04 04:07:29 +0000

    Created a function to test if two arrays are ===

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+let emoji = String.fromCodePoint(128512);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = function(a, b) {
+  console.log(a,b);
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
