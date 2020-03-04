97fef6248c3e2a3055611d5f2f20223907152298
Taha Rizvi <riztaha@gmail.com>
2020-03-04 18:33:22 +0000

    Removed assertions and changed it into a function.

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(a, b) {
+ // console.log(a,b);
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a,b)) { console.log("The two arrays are equal!")}
+  else {console.log("The two arrays are NOT EQUAL!")}
+}
+
+// console.log(eqArrays([1,2,3], [1,2,5]))
+assertArraysEqual([1,2,3], [1,2,5]) // --> false
+assertArraysEqual([1,2,3], [1,2,3]) // --> true
+assertArraysEqual([1,2,3], [1,2,"3"]) // --> false
