a86893a428dae35399012cec1f9822cd3db67213
Taha Rizvi <riztaha@gmail.com>
2020-03-04 22:56:51 +0000

    Code Reviewed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,3 +20,4 @@ const assertArraysEqual = function(a, b) {
 assertArraysEqual([1,2,3], [1,2,5]) // --> false
 assertArraysEqual([1,2,3], [1,2,3]) // --> true
 assertArraysEqual([1,2,3], [1,2,"3"]) // --> false
+
