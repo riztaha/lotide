564e94a1dfb8cd3c1f59afc7158b16dc05867208
Taha Rizvi <riztaha@gmail.com>
2020-03-05 17:25:39 +0000

    In an object, finding the key when the value argument is given.

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+//Assert Equal Function
+let emoji = String.fromCodePoint(128512);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//findKeyByValue code:
+const findKeyByValue = function (objectName, valueName) {
+  let keys
+  keysInObject = Object.keys(objectName)
+  for (let key of keysInObject) {
+    let valueOfKey = objectName[key]
+    if (valueName === valueOfKey) {
+      return key
+      }
+    // console.log(valueOfKey)
+    // console.log(key)
+    }
+}
+
+
+
+
+//Assertions:
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
