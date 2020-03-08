793099ef79c14df8612976ccd197e74e15821282
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-03-08 12:01:48 -0400

    Added pseudocode to make it easy to understand

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,5 @@
 let emoji = String.fromCodePoint(128512);
-
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
   } else {
@@ -8,7 +7,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (a, b) {
+const eqArrays = function(a, b) {
   // console.log(a,b);
   if (a.length !== b.length) {
     return false;
@@ -24,7 +23,7 @@ const eqArrays = function (a, b) {
 // eqObjects function
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   // Store keys of each object
   let object1Key = Object.keys(object1);
   let object2Key = Object.keys(object2);
@@ -33,17 +32,17 @@ const eqObjects = function (object1, object2) {
   if (object1Key.length !== object2Key.length) {
     return false;
   } else {
-
-// Loop through the object keys for each value.
+    // Loop through the object keys for each value.
     for (let i = 0; i < object1Key.length; i++) {
     // if (object1Key[i] === object2Key[i]) {
-// If the Value is an array, 
+      // If the Value is an array,
       if (Array.isArray(object1Key[i])) {
-// Then run the eqArrays fn on the values to see if they are equal.
-        return eqArrays(object1[i], object2[i])
+        // Then run the eqArrays fn on the values to see if they are equal.
+        return eqArrays(object1[i], object2[i]);
       }
-      else if(object1[i] !== object2[i]) {
-        return false
+      // Otherwise compare the value of the keys
+      else if (object1[i] !== object2[i]) {
+        return false;
       }
       return true;
     }
