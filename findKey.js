56522ce011d354c63167a1717975c17d9220ad5a
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-03-08 12:02:32 -0400

    Created a findKey with a callback function function(object, callback)

--- findKey.js
+++ findKey.js
@@ -9,4 +9,34 @@ const assertEqual = function(actual, expected) {
 };
 
 //findKey Implementation:
+const findKey = function (object, callback) {
+  for (key in object) {
+    let value = object[key];
+    // console.log(value) // --> { stars: 1} 
+    if (callback(value) ) {
+      return key
+    }
+  }
+}
 
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+let obj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+
+// let call = (x => x.stars === 2)
+// console.log(x)
