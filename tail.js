7705e86a04769e502086ec03401c5a17b09b6199
Taha Rizvi <riztaha@gmail.com>
2020-03-09 18:00:16 +0000

    Refactored test cases into test file.

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-// let emoji = String.fromCodePoint(128512);
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
+
 const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
