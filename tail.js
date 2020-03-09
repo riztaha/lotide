be9c3ae35e9ca28994d2c721c7966d1ccf27099b
Taha Rizvi <TahaRizvi@Tahas-MacBook-Pro.local>
2020-03-09 13:22:34 -0400

    Refactored & added test files.

--- tail.js
+++ tail.js
@@ -1,12 +1,12 @@
-let emoji = String.fromCodePoint(128512);
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
+// let emoji = String.fromCodePoint(128512);
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(emoji + ' ' + emoji + ' ' + emoji + ` Assertion Passed: [${actual}] === [${expected}]`);
+//   } else {
+//     console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   const newArr = [];
@@ -20,11 +20,6 @@ const tail = function(arr) {
 };
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail
